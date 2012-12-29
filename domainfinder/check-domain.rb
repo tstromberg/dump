fd0c4958cebe0f979b35a8177b2658ad09c6795c
Thomas Stromberg <thomas+git@stromberg.org>
2012-12-29 09:51:29 -0800

    Update for new domains

--- domainfinder/check-domain.rb
+++ domainfinder/check-domain.rb
@@ -2,79 +2,90 @@
 $CACHE_DIR = ENV['HOME'] + '/.domainfinder/'
 
 # .org only allows 4 per minute
-min_server_delay = (ARGV[1] or 20).to_i
+# .at is even stricter
+min_server_delay = (ARGV[1] or 25).to_i
 
 available_keywords = [
   ' - Available',
-  'Not found',
-  '\" not found\.',
-  'We do not have an entry in our database matching your query',
-  'Not Registered - ',
-  'no entries found',
-  'No entries found',
-  'is free',
-  'No information about domain',
-  'is not registered',
-  'nothing found',
-  'not found in',
-  'No match',
-  'NOT FOUND',
-  'Status:             AVAILABLE',
-  'Status:      FREE',
-  'NOMATCH',
-  'No Data Found',
-  'No data found',
-  'No such domain',
   'Domain Not Found',
-  'Status:			available', #lt
+  'Domain status:\s+available', # ca
   'NO MATCH for domain',
+  'NOMATCH',
+  'NOT FOUND',
+  'No Data Found',
   'No Found', # tw
+  'No Match for', # bz
+  'No data found',
+  'No entries found',
+  'No information about domain',
+  'No information available about domain name', # pl
+  'No match',
+  'No such domain',
+  'Not Registered - ',
+  'Not found',
+  'Object_Not_Found', # mx
+  'Status: Not Registered', # cm
+  'Status:\s+AVAILABLE', # be
+  'Status:\s+FREE',
+  'Status:\s+available', # lt
+  'Status:\s+free', # de
+  'We do not have an entry in our database matching your query',
+  '\" not found\.',
+  'domain name not known', # tk
+  'is free',
   'is not a valid domain name', # de
-  'Status:         AVAIL', # ca
+  'is not registered',
+  'no entries found',
   'no matching record', # cn
-  'Not found:', # so
+  'not found in',
+  'not found\.\.\.', # tc
+  'nothing found',
 ]
 
 taken_keywords = [
-  'Status:         EXIST',
-  'Status:      QUARANTINE', #be
+  'Above domain name is not available', # kr
+  'Admin-email', # sk
+  'Administrative Contact:', #bz
+  'Current Nameservers',
+  'Domain ID:', # mobi
+  'Domain Name ID', # au
+  'Domain nameservers:',
+  'Domain servers',
+  'Domain status:\s+unavailable', # new ca
+  'ERR - Nothing found',
+  'Holder of domain name', # li
   'NS1-Hostname',
   'Name Server',
-  'Status:      REGISTERED',
-  'Nameservers',
-  'RESERVED NAME',
-  'Domain nameservers:',
-  'registrant\'s handle:',
-  'dns_name',
-  'Record created on ',
   'Name servers:',
-  'Current Nameservers',
-  'nameserver:',
-  'Registered Date', #kr, generic
-  'Technical Contact',
+  'Nameserver:',
+  'Nameservers',
   'Not available',
+  'Nserver:',
+  'RESERVED NAME',
+  'Record created on ',
+  'Registered Date', #kr, generic
+  'Registrant ID:', #so
+  'Registrant Name: ', #cn
+  'Status:\s+EXIST',
+  'Status:\s+UNAV', # ca
+  'Status:\s+QUARANTINE', #be
+  'Status:\s+REGISTERED',
+  'Tech ID:', # mobi
+  'Technical Contact',
+  'Technical contact:',
+  'The domain you requested is in the reserved list', # cn
+  '\[Tech-C\]', # de
+  '\d\.\d+\.\d+\.\d+.*\w+\.\w+.*\d\d+\.\d+\.\d+\.\d', # to
+  'dns_name',
   'domain_pri_ns:',
   'is in the queue for registration',
-  'Nameserver:',
-  'registrar:', # ru
-  'Nserver:',
-  'Admin-email', # sk
-  'Above domain name is not available', # kr
-  'Technical contact:',
+  'nameserver:',
   'nserver:',
-  'Domain servers',
-  'Registrant Name: ', #cn
-  'The domain you requested is in the reserved list', # cn
+  'registrant type:', # pl
+  'registrant\'s handle:',
+  'registrar:', # ru
   'reserved name', # tw
   'tech-c:', # lv
-  'Tech ID:', # mobi
-  'Domain ID:', # mobi
-  'Tech-C', # de
-  'ERR - Nothing found',
-  'Domain Name ID', # au
-  'Holder of domain name', # li
-  'Status:         UNAV', # ca
-  '\d\.\d+\.\d+\.\d+.*\w+\.\w+.*\d\d+\.\d+\.\d+\.\d', # to
 ]
 
 $available_regexp = available_keywords.join('|')
@@ -92,9 +103,14 @@ last_server_ts = {}
 
 def determine_whois_server(full_domain)
   (domain, tld) = full_domain.split('.')
+  # TODO: Read in a file like http://www.nirsoft.net/whois_servers_list.html
+
   whois_server = case tld
-    when 'tc','ms','vg' then 'whois.adamsnames.tc'
-    when 'la','ly', 'lv', 'so' then 'whois.nic.' + tld
+    when 'tc', 'tc', 'vg' then 'whois.adamsnames.tc'
+    when 'bz' then 'whois.belizenic.bz'
+    when 'cm' then 'whois.netcom.cm'
+    when 'de' then 'whois.denic.de'
+    when 'la','ly', 'lv', 'me', 'so' then 'whois.nic.' + tld
     else tld + '.whois-servers.net'
   end
   return whois_server
@@ -105,7 +121,7 @@ def whois(full_domain, whois_server)
 
   if output =~ /#{$available_regexp}/ && output =~ /#{$taken_regexp}/
     status='Unknown (matches both!)'
-  elsif output =~ /#{$available_regexp}/ 
+  elsif output =~ /#{$available_regexp}/
     status='Available'
   elsif output =~ /#{$taken_regexp}/
     status='Taken'
@@ -113,16 +129,16 @@ def whois(full_domain, whois_server)
     status='Invalid'
   else
     status='Unknown'
-  end  
-  
+  end
+
   return status, output
 end
-  
+
 
 
 domains.each do |next_domain|
   full_domain = next_domain.chomp
-  cachefile = $CACHE_DIR + full_domain 
+  cachefile = $CACHE_DIR + full_domain
   if File.exists?(cachefile)
     f = File.new(cachefile)
     status = f.readlines[0].chomp
@@ -137,10 +153,10 @@ domains.each do |next_domain|
         required_sleep = min_server_delay - difference
         sleep(required_sleep)
       end
-    end 
+    end
     (status, output) = whois(full_domain, whois_server)
     last_server_ts[whois_server] = Time.now.to_i
-    
+
     # update cache
     if status != 'Unknown'
       f = File.new(cachefile, 'w')
@@ -150,9 +166,9 @@ domains.each do |next_domain|
   end
   print "#{status}: #{full_domain}"
   if status == 'Unknown':
-    print "\t# %-90.90s\n" % output.gsub(/[\r\n]/, '')
+    print "\t\t# #{whois_server}: %s\n" % output.gsub(/[\r\n]/, '')
   else
     puts
   end
-  $stdout.flush  
+  $stdout.flush
 end
